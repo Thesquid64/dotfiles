2852e9732ae1a547ea20d148744ec8a00bde23f1
Thesquid64 <max.craft640@gmail.com>
2026-04-21 12:41:18 +0200

    virt-manager instead of virtmanager in voidscript

--- scripts/voidScript.sh
+++ scripts/voidScript.sh
@@ -1,6 +1,6 @@
 sudo xbps-install -u xbps
 sudo xbps-install -Syu
-sudo xbps-install vim neovim git firefox alacritty picom fzf sddm btop kew feh pfetch qalc dbus rofi krita pavucontrol scrcpy onboard virtmanager qemu wget xorg-minimal make gcc libX11-devel libXft-devel libXinerama-devel libXfixes-devel libXi-devel pkg-config dbus unzip 
+sudo xbps-install vim neovim git firefox alacritty picom fzf sddm btop kew feh pfetch qalc dbus rofi krita pavucontrol scrcpy onboard virt-manager qemu wget xorg-minimal make gcc libX11-devel libXft-devel libXinerama-devel libXfixes-devel libXi-devel pkg-config dbus unzip 
 
 cd
 mkdir scripts git temp
